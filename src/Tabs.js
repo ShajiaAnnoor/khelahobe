51f6fb805583676820e9fbfc10496b47905663da
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-15 20:03:14 +0600

    chore: add proper icons

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign } from "@expo/vector-icons";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Home from "./screens/Home";
 //import Details from "./screens/ScoreDetails";
@@ -12,33 +14,38 @@ import RankingScreen from "./screens/Ranking/RankingScreen";
 import BlogList from "./screens/Blog/BlogList";
 import Videos from "./screens/Videos/VideoList/index";
 //import SeriesScreen from "./screens/"
+import { Icon } from '@rneui/themed';
 
 const Tab = createBottomTabNavigator()
 
 export default function Tabs() {
     return (
         <Tab.Navigator
-            initialRouteName="BlogList"
+            initialRouteName="Home"
             tabBarOptions={{
-                activeTintColor: '#e91e63',
+                activeTintColor: 'green',
             }}
             screenOptions={{headerShown:false}}
             options={{
-                headerTintColor: 'red',
+                headerTintColor: 'blue',
                 headerStyle: { backgroundColor: 'tomato' },
             }}
         >
 
+            {/*
             <Tab.Screen 
                 name="ScorecardScreen" 
                 component={ScorecardScreen} 
                 options={{
                     tabBarLabel: "ScorecardScreen",
                     tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="notification" size={24} color={color} />
-                      ),
+                        <Icon
+                            name='g-translate'
+                            color='#00aced' 
+                        />                      ),
                 }}
             />
+            */}
           {/* <Tab.Screen 
                 name="MatchDetails" 
                 component={MatchDetails} 
@@ -55,7 +62,12 @@ export default function Tabs() {
                 options={{
                     tabBarLabel: "Home",
                     tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="notification" size={24} color={color} />
+                        <Icon 
+                            name="home" 
+                            color={color}
+                            size={30}
+                            type="material"
+                        />
                       ),
                 }}
             />
@@ -93,7 +105,7 @@ export default function Tabs() {
                       ),
                 }} 
             />*/}
-
+            {/*
             <Tab.Screen 
                 name="Livescore" 
                 component={Livescore}
@@ -104,14 +116,19 @@ export default function Tabs() {
                       ),
                 }} 
             />
+            */}
             <Tab.Screen 
                 name="Videos" 
                 component={Videos}
                 options={{
                     tabBarLabel: "Videos",
                     tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="notification" size={24} color={color} />
-                      ),
+                        <Icon 
+                            name="ondemand-video" 
+                            color={color}
+                            size={30}
+                            type="material"
+                        />                      ),
                 }}  
             />
             <Tab.Screen 
@@ -120,8 +137,12 @@ export default function Tabs() {
                 options={{
                     tabBarLabel: "BlogList",
                     tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="notification" size={24} color={color} />
-                      ),
+                        <Icon 
+                            name="auto-stories" 
+                            color={color}
+                            size={30}
+                            type="material"
+                        />                      ),
                 }} 
             />
             {/*
