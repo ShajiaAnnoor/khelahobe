a7c28ecdcd14f1329e8a8d2048e212015ac5b81e
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-17 01:24:43 +0600

    feat: add splash screen for android

--- App.tsx
+++ App.tsx
@@ -5,13 +5,25 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
+import BootSplash from "react-native-bootsplash";
 import { store, persistor } from "./redux/store";
 import MyApp from "./src/router";
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
 
   return (
     <Provider store={store}>
