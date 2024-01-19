024a56477c8380a0e7683cf0b8b9d51d660ed823
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-19 17:55:20 +0600

    chore: refactor style and utility function to other files

--- src/screens/Home/LivescoreInfo/index.js
+++ src/screens/Home/LivescoreInfo/index.js
@@ -8,7 +8,6 @@ import {
 } from "react-redux";
 import { 
   Pressable, 
-  StyleSheet, 
   Text, 
   View,
 } from "react-native";
@@ -23,133 +22,8 @@ import {
 } from "../../../../redux/reducers";
 import { fetchMatchWithInnings } from "../../../../redux/complex-actions/scoreboard";
 import { dfs } from "../../../API/utils";
-//import ScorecardScreen from "../../Scorecard/ScorecardScreen";
-
-const months = {
-  'January': 'জানুয়ারি',
-  'February': 'ফেব্রুয়ারি',
-  'March': 'মার্চ',
-  'April': 'এপ্রিল',
-  'May': 'মে',
-  'June': 'জুন',
-  'July': 'জুলাই',
-  'August': 'আগস্ট',
-  'September': 'সেপ্টেম্বর',
-  'October': 'অক্টোবর',
-  'November': 'নভেম্বর',
-  'December': 'ডিসেম্বর',
-  'January,': 'জানুয়ারি,',
-  'February,': 'ফেব্রুয়ারি,',
-  'March,': 'মার্চ,',
-  'April,': 'এপ্রিল,',
-  'May,': 'মে,',
-  'June,': 'জুন,',
-  'July,': 'জুলাই,',
-  'August,': 'আগস্ট,',
-  'September,': 'সেপ্টেম্বর,',
-  'October,': 'অক্টোবর,',
-  'November,': 'নভেম্বর,',
-  'December,': 'ডিসেম্বর,',
-};
-
-function giveBelaOfTheDay(start_time_of_match_hours) {
-  let h = "";
-  if (
-    (
-      start_time_of_match_hours >= 0 && start_time_of_match_hours < 3
-    )
-    ||
-    (
-      start_time_of_match_hours >= 20 && start_time_of_match_hours <= 24
-    )
-  ) {
-      h = 'রাত';
-  }
-  else if (start_time_of_match_hours === 0) {
-      h = 'রাত';
-  }
-  else if (start_time_of_match_hours < 7 && start_time_of_match_hours >= 3) {
-      h = 'ভোর';
-  }
-  else if (start_time_of_match_hours >= 7 && start_time_of_match_hours < 11) {
-      h = 'সকাল';
-  }
-  else if (start_time_of_match_hours >= 11 && start_time_of_match_hours < 12) {
-      h = 'বেলা';
-  }
-  else if (start_time_of_match_hours>= 12 && start_time_of_match_hours< 15) {
-      h = 'দুপুর';
-  }
-  else if (start_time_of_match_hours>= 15 && start_time_of_match_hours< 18) {
-      h = 'বিকাল';
-  }
-  else if (start_time_of_match_hours>= 18 && start_time_of_match_hours< 20) {
-      h = 'সন্ধ্যা';
-  }
-
-  return h ; 
-}
-
-function getTime(start_time, day) {
-
-  let today = new Date();
-  let [days, month, year] = day[0].split(" ");
-  let todayOrNotToday = (today.getDate()) === days ? ('আজ ' + days) : days;
-  let start_time_of_match = new Date(start_time);
-  let start_time_of_match_hours = start_time_of_match.getHours();
-  let h = '', test = '', st = '';
-  let blabla = day[0].split(" ");
-
-  if (blabla[2]) {
-      for (let i in blabla) {
-          if (months[blabla[i]]) {
-              test += months[blabla[i]] + ' ';
-          }
-          else {
-              test += blabla[i] + ' ';
-          }
-      }
-  }
-  else {
-
-  }
-
-  h = giveBelaOfTheDay(start_time_of_match_hours);
-  h += ' ' + (
-      dfs(
-        start_time_of_match_hours> 12
-              ?
-              (
-                start_time_of_match_hours- 12
-              )
-              :
-              (
-                  (
-                    start_time_of_match_hours=== 0
-                      &&
-                      '12'
-                  )
-                  ||
-                  start_time_of_match.getHours()
-              )
-      )
-  ) + '.' + dfs(
-      (
-        start_time_of_match.getMinutes() > 0
-              ?
-              start_time_of_match.getMinutes()
-              :
-              (
-                  '0' + start_time_of_match.getMinutes()
-              )
-      )
-  ) + 'টায়';
-
-  test = test.slice(0, -1);
-
-  st = (test !== '' && `${test}, ${h}`) || (`${todayOrNotToday} ${months[month]} ${year}, ${h}`);
-  return st;
-}
+import { styles } from "./style";
+import { getTime } from "./utils";
 
 const LiveScoreInfo = ({
     slug, flag
@@ -303,22 +177,23 @@ const LiveScoreInfo = ({
 	}
 
 	const l = `${shortScoreCard.remainingball} বলে ${shortScoreCard.remainingrun} রান দরকার`;
-  const st = getTime(data.start_time, data.days);
+	const st = getTime(data.start_time, data.days);
 
 	if (!data) return '';
+	
 	let over = teamscore[0] && teamscore[0].over || ''
 	let out = teamscore[0] && teamscore[0].out || ''
 
 	if ( out !== '১০' ) { out = `/${out}`}
-  else { out = "" }
+	else { out = "" }
 
 	let over1 = teamscore[1] && teamscore[1].over || '';
 	let out1 = teamscore[1] && teamscore[1].out || '';
 
 	if ( out1 !== '১০' ) { out1 = `/${out1}`}
-  else { out1 = "" };
+	else { out1 = "" };
 
-  const navigation=useNavigation();
+	const navigation=useNavigation();
 
     return (
 		<View style={styles.listItem}>
@@ -366,8 +241,7 @@ const LiveScoreInfo = ({
 								`${out} (${over} ওভার)`
 								)
 								:
-								null
-							
+								null							
 							)
 						}
 						{(data.matchType === "TEST" && teamscore[test1] && teamscore[test1].total) || ''}
@@ -478,16 +352,7 @@ const LiveScoreInfo = ({
 
         <View style={{paddingLeft:1,padding:2,flex:2}}>
 			<Pressable 
-				style ={{
-					height: 40,
-					width:100,
-					borderRadius:10,
-					backgroundColor:"green",
-					padding:10,
-					marginTop :1,
-					marginBottom:5,
-					elevation: 10,
-				}}
+				style ={styles.buttonMatchDetails}
 				onPress={()=>navigation.navigate('ScorecardScreen',{slug:slug})}
 			>
 				<Text style={styles.buttonTextStyle}>
@@ -497,194 +362,5 @@ const LiveScoreInfo = ({
         </View>
     </View>
 )}
-
-export const styles = StyleSheet.create({
-	pageContainer: {
-		flex:1,
-		flexDirection:'column',
-		margin:5,
-		paddingTop:5,
-		justifyContent: 'space-evenly',
-	},
-   
-	teamScoreItemContainer: {
-		flex:1,
-		flexDirection:'row',
-		justifyContent: 'space-between',
-		alignContent:'space-between',
-		paddingLeft:15,
-		width:'100%',
-		height:"10%",
-		flexWrap:'wrap',
-	},
-
-	itemContainer: {
-		flexDirection:'column',
-		justifyContent: 'space-evenly',
-		alignItems:'flex-start',
-		width:"100%",
-		height:'25%',
-		margin:1,
-		padding:2,
-		borderRadius:10,
-		paddingTop:5,
-		paddingBottom:5,
-	},
-
-	listItem: {
-		margin: 2,
-		marginTop:10,
-		marginRight:8,
-		padding:10,
-		paddingBottom:2,
-		paddingTop:2,
-		backgroundColor:'white',
-		height:'10%',
-		width: '98%',
-		flex:1,
-		flexDirection: 'column',
-		borderRadius: 10,
-		borderBottomColor:'green',
-		borderBottomWidth:2,
-		borderLeftWidth:2,
-		borderLeftColor:'green',
-		alignItems:'flex-start',
-		justifyContent: 'space-evenly',
-		alignItems:'flex-start',
-		alignContent:'space-around',
-	},  
-
-	buttonTextStyle:{
-		color: "white",
-		fontWeight: "bold",
-		textAlign: "center",
-		fontSize:10,
-		flexWrap:'wrap',
-		justifyContent:'center',
-		padding:4,
-	},
-
-	scoreContainer:{
-		height:35,        
-		paddingLeft:30,
-		marginLeft:10,
-		flexDirection:"column",
-		margin:2,
-		padding:2,
-		justifyContent:'center',
-		alignSelf:'flex-end',
-		alignContent:'flex-end',
-		position:'relative',        
-	},
-
-	countryNameStyle:{
-		height:35,
-		margin:2,
-			padding:2,
-		justifyContent:'space-evenly',
-		position:'relative',        
-	},
-
-	scoreTextStyle:{ 
-		fontWeight: 'bold', 
-		textAlign:"right",
-		fontSize:12,
-		backgroundColor:"white",
-		justifyContent:'flex-end',
-		alignItems:'flex-end',
-	},
-
-	countryNameTextStyle: {
-		fontWeight: 'bold',
-		textAlign:"left",
-		fontSize:12,
-		backgroundColor:'white',
-		justifyContent:'flex-start',
-	},
-
-	countryFlagStyle:{
-		height:20,
-		width:1,
-		margin:5,
-		marginRight:2,
-		padding:2,
-		paddingRight:3,
-		flex:1,
-		alignItems:'flex-end',
-	},
-
-	summaryContainer:{
-		width:"100%",
-		paddingLeft:2,
-		margin:2,
-		flexBasis:"2%",
-		padding:2,
-		flex:2,
-		alignItems:"flex-start",
-		flexWrap:'wrap',
-	},
-
-	summaryText:{
-		fontWeight: 'bold',
-		textAlign:"left",
-		fontSize:12,
-		color:"green",
-		backgroundColor:'white',
-		margin:2,
-		padding:1,
-		paddingLeft:4,
-		alignItems:"center",
-	},
-
-	dateAndStatusContainer:{
-		flexDirection:'row',
-		padding:2,
-		width:'100%',
-	},
-
-	manOfTheMatchContainer:{
-		flexDirection:'row',
-		width:'90%',
-		padding:2,
-		backgroundColor:'white'
-	},
-
-	statusText:{
-		fontWeight:'bold', 
-		margin:2,
-		fontSize:12,
-		textAlign:'left',
-		color:'green',
-	},
-
-	dateTimeText:{
-		fontWeight:'bold', 
-		margin:2,
-		fontSize:12,
-		textAlign:'left',
-		width:"100%",
-	},
-
-	manOfTheMatchText:{
-		fontWeight:'bold', 
-		margin:2,
-		fontSize:12,
-		textAlign:'left',
-	},
-
-	manOfTheMatchNameText:{
-		fontWeight:'bold', 
-		margin:2,
-		fontSize:12,
-		color:'green',
-		textAlign:'left',
-	},
-
-	scrollView: {
-		backgroundColor: 'white',
-		marginHorizontal: 20,
-	},
-  }
-);
 
 export default LiveScoreInfo;
