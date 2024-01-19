23be6ac4388a6317fd7c881296a2d9fdf5dfc784
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-19 19:46:42 +0600

    refactor: fix indentation

--- src/screens/Scorecard/Squad/index.js
+++ src/screens/Scorecard/Squad/index.js
@@ -1,159 +1,159 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import uuid from 'react-native-uuid';
 import _ from "lodash"
 
 export default function App({teams}) {
-  
-  const tableHeader = (teamname, alignDir,paddingL,paddingR) => {
-    console.log(alignDir)
-    return (
-      <View style={styles.tableHeader}>
-                  
-            <Text style={{...styles.tableHeaderText,color:'white',textAlign:alignDir,paddingLeft:paddingL,paddingRight:paddingR}}>{teamname}</Text>
-              
-      </View>
-    );
-  }
-  
-  return (
-    (teams[0] && teams[1]&&
-    <View style={styles.container}>
-      <FlatList 
-        data={teams[0]}
-        style={{flexDirection:'column',flex:2, width:"50%"}}
-        keyExtractor={(item, index) => uuid.v4()}
-        ListHeaderComponent={tableHeader(teams[0].name, "right",0,5)}
-        //stickyHeaderIndices={[0]}
-        renderItem={({item, index})=> {
-          return (
-            <View style={{...styles.eachTableRowView, backgroundColor: index % 2 == 1 ? "white" : "#F0FBFC"}}>
-                <View style={styles.team1PlayerNames}>
-                    <Text numberOfLines={1} style={{...styles.team1PlayerNamesText, fontWeight:"bold",}}>{item}</Text>
-                </View>
-            </View>
-          )
-        }}
-      />
 
-    <FlatList 
-        data={teams[1]}
-        style={{flexDirection:'column',flex:3,width:"50%"}}
-        keyExtractor={(item, index) => uuid.v4()}
-        ListHeaderComponent={tableHeader(teams[1].name, "left",5,0)}
-        //stickyHeaderIndices={[0]}
-        renderItem={({item, index})=> {
-          return (
-            <View style={{...styles.eachTableRowView, backgroundColor: index % 2 == 1 ? "white" : "#F0FBFC"}}>
-                <View style={styles.team2PlayerNames}>
-                    <Text numberOfLines={1} style={{...styles.team2PlayerNamesText, fontWeight:"bold",}}>{item}</Text>
-                </View>
-            </View>
-          )
-        }}
-      />
-      <StatusBar style="auto" />
-    </View>)
-  );
+	const tableHeader = (teamname, alignDir,paddingL,paddingR) => {
+		return (
+			<View style={styles.tableHeader}>          
+				<Text style={
+					{
+						...styles.tableHeaderText,
+						color:'white',
+						textAlign:alignDir,
+						paddingLeft:paddingL,
+						paddingRight:paddingR,
+					}
+				}>
+					{teamname}
+				</Text>
+			</View>
+		);
+  	}
+  
+  	return (
+		(teams[0] && teams[1]&&
+			<View style={styles.container}>
+				<FlatList 
+					data={teams[0]}
+					style={{flexDirection:'column',flex:2, width:"50%"}}
+					keyExtractor={() => uuid.v4()}
+					ListHeaderComponent={tableHeader(teams[0].name, "right",0,5)}
+					renderItem={({item, index})=> {
+						return (
+						<View style={{...styles.eachTableRowView, backgroundColor: index % 2 == 1 ? "white" : "#F0FBFC"}}>
+							<View style={styles.team1PlayerNames}>
+								<Text numberOfLines={1} style={{...styles.team1PlayerNamesText, fontWeight:"bold",}}>{item}</Text>
+							</View>
+						</View>
+						)
+					}}
+				/>
+
+				<FlatList 
+					data={teams[1]}
+					style={{flexDirection:'column',flex:3,width:"50%"}}
+					keyExtractor={() => uuid.v4()}
+					ListHeaderComponent={tableHeader(teams[1].name, "left",5,0)}
+					//stickyHeaderIndices={[0]}
+					renderItem={({item, index})=> {
+						return (
+						<View style={{...styles.eachTableRowView, backgroundColor: index % 2 == 1 ? "white" : "#F0FBFC"}}>
+							<View style={styles.team2PlayerNames}>
+								<Text numberOfLines={1} style={{...styles.team2PlayerNamesText, fontWeight:"bold",}}>
+									{item}
+								</Text>
+							</View>
+						</View>
+						)
+					}}
+				/>
+				<StatusBar style="auto" />
+			</View>
+		)
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop:15,
-    margin:2,
-    padding:2,
-    flexDirection:'row',
-  },
-  tableHeader: {
-    backgroundColor:'green',
-    borderTopEndRadius: 5,
-    borderTopStartRadius: 5,
-    
-    flexWrap:'nowrap',
-    height:"10%",
-    width:"100%",
-   paddingTop:10,
-   paddingBottom:10,
-   paddingLeft:2,
-    flex:3,
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop:15,
+		margin:2,
+		padding:2,
+		flexDirection:'row',
+	},
 
-  tableHeaderText:{
-    color: "white",
-    fontWeight:"bold",
-    textAlign:'center',
+	tableHeader: {
+		backgroundColor:'green',
+		borderTopEndRadius: 5,
+		borderTopStartRadius: 5,
 
-  },
+		flexWrap:'nowrap',
+		height:"10%",
+		width:"100%",
+		paddingTop:10,
+		paddingBottom:10,
+		paddingLeft:2,
+		flex:3,
+	},
 
-  tableRow: {
-    flexDirection: "row",
-    height: 40,
-    alignItems:"center",
-    flexWrap:"wrap",
-  },
-  
+	tableHeaderText:{
+		color: "white",
+		fontWeight:"bold",
+		textAlign:'center',
+	},
 
-  team1PlayerNames: {
-     width:'100%',
-    alignItems:"flex-end",
-    height:35,
-    marginRight:2,
-    paddingRight:5,
-    margin:1,
-    padding:1,
-    //justifyContent:'center',
-    flexWrap:"nowrap", 
-    flex:2,
-    
-  },
+	tableRow: {
+		flexDirection: "row",
+		height: 40,
+		alignItems:"center",
+		flexWrap:"wrap",
+	},
 
-  team1PlayerNamesText: {
-    //width:"100%",
-   // height:"100%",
-    fontWeight: 'bold',
-    textAlign:"right",
-    fontSize:8,
-    flexWrap:'nowrap', 
-    padding:2,
-    margin:2,
-},
 
-team2PlayerNames: {
-    width:'100%',
-    alignItems:"flex-start",
-    paddingLeft:5,
-    marginLeft:2,
-    height:35,
-    margin:1,
-    padding:1,
-    flexWrap:'nowrap',
-    flex:2,
-  },
+	team1PlayerNames: {
+		width:'100%',
+		alignItems:"flex-end",
+		height:35,
+		marginRight:2,
+		paddingRight:5,
+		margin:1,
+		padding:1,
+		flexWrap:"nowrap", 
+		flex:2,
+	},
 
-  team2PlayerNamesText: {
-   // width:"100%",
-    //height:"100%",
-    fontWeight: 'bold',
-    textAlign:"left",
-    fontSize:8,
-   // paddingLeft:8,
-   padding:2,
-   margin:2,
-    flexWrap:'nowrap',   
-    //backgroundColor:'yellow',  
-},
-  
-  eachTableRowView: {
-    flex:2,
-    flexDirection:'row',
-    justifyContent:'center',
-    flexWrap:'nowrap',
-    height:"10%",
-    width:"100%",
-  },
+	team1PlayerNamesText: {
+		fontWeight: 'bold',
+		textAlign:"right",
+		fontSize:8,
+		flexWrap:'nowrap', 
+		padding:2,
+		margin:2,
+	},
+
+	team2PlayerNames: {
+		width:'100%',
+		alignItems:"flex-start",
+		paddingLeft:5,
+		marginLeft:2,
+		height:35,
+		margin:1,
+		padding:1,
+		flexWrap:'nowrap',
+		flex:2,
+	},
+
+	team2PlayerNamesText: {
+		fontWeight: 'bold',
+		textAlign:"left",
+		fontSize:8,
+		padding:2,
+		margin:2,
+		flexWrap:'nowrap',   
+	},
+
+	eachTableRowView: {
+		flex:2,
+		flexDirection:'row',
+		justifyContent:'center',
+		flexWrap:'nowrap',
+		height:"10%",
+		width:"100%",
+	},
 });
