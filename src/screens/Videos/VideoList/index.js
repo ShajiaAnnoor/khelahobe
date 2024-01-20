2f87726be0ba250e87ce3c67c2b2e0bfb969e8ff
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-20 12:32:02 +0600

    refactor: move style to another file

--- src/screens/Videos/VideoList/index.js
+++ src/screens/Videos/VideoList/index.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useCallback, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import YoutubePlayer from "react-native-youtube-iframe";
 
 import { fetchHighlights } from "../../../../redux/complex-actions/highlights";
@@ -38,7 +38,11 @@ export default function App() {
 		<View style={styles.container}>
 			<FlatList 
 				data={highlights}
-				style={{width:"100%",flex:1,flexDirection:'column'}}
+				style={{
+					width:"100%",
+					flex:1,
+					flexDirection:'column',
+				}}
 				keyExtractor={(index) => index+""}
 				renderItem={({item})=> {
 					return (
@@ -49,7 +53,9 @@ export default function App() {
 								videoId={item.youtube_id}        
 								onChangeState={onStateChange}      
 							/>  
-							<Text style={styles.headline}>{item.description.substring(0,100)}</Text>    
+							<Text style={styles.headline}>
+								{item.description.substring(0,100)}
+							</Text>    
 						</View> 
 					)
 				}}
@@ -57,22 +63,3 @@ export default function App() {
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingTop:15,
-		margin:2,
-		padding:10,
-	},
-
-	headline: {
-		fontSize:20,
-		fontWeight:'bold',
-		paddingTop:2,
-		marginTop:0,
-		marginBottom:30,
-	}
-});
