d3a9bda3e8d39669524215d42f816f23fdcde2c1
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-20 12:30:05 +0600

    refactor: fix indentation

--- src/screens/Scorecard/MatchDetails/index.js
+++ src/screens/Scorecard/MatchDetails/index.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import _ from "lodash"
 import uuid from 'react-native-uuid';
+
+import { styles } from './style';
 
 export default function App({
 	days,
@@ -121,95 +123,3 @@ export default function App({
 		</View>
   );
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingTop:15,
-		margin:2,
-		padding:10,    
-	},
-
-	tableHeader: {
-		backgroundColor:'green',
-		flexWrap:'nowrap',
-		height:"10%",
-		width:"100%",
-		paddingTop:10,
-		paddingBottom:10,
-		paddingLeft:2,
-		flex:3,
-	},
-
-	tableHeaderText:{
-		color: "white",
-		fontWeight:"bold",
-		textAlign:'center',
-	},
-
-	tableRow: {
-		flexDirection: "row",
-		height: 40,
-		alignItems:"center",
-		flexWrap:"wrap",
-	},
-
-	rowItemName: {
-		alignItems:"center",
-		height:35,
-		margin:2,
-		padding:2,
-		justifyContent:'center',
-		flexWrap:"nowrap", 
-		flex:3,
-		flexShrink:1,
-	},
-
-	rowItemNameText: {
-		width:"100%",
-		height:"100%",
-		fontWeight: 'bold',
-		textAlign:"left",
-		fontSize:10,
-		paddingLeft:8,
-		justifyContent:'center',
-		flexWrap:'nowrap',
-		alignContent:'center',
-		flexDirection:'column',     
-	},
-
-	rowItemValue: {
-		alignItems:"center",
-		height:35,
-		margin:2,
-		padding:2,
-		justifyContent:'center',
-		flexWrap:"nowrap", 
-		flex:7,
-		flexShrink:1,
-	},
-
-	rowItemValueText: {
-		width:"100%",
-		height:"100%",
-		fontWeight: 'bold',
-		textAlign:"center",
-		fontSize:10,
-		justifyContent:'center',
-		flexWrap:'nowrap',
-		alignContent:'center',
-		flexDirection:'column',     
-	},
-
-	eachTableRowView: {
-		flex:2,
-		flexDirection:'row',
-		justifyContent:'space-between',
-		alignItems:'center',
-		flexWrap:'wrap',
-		height:"10%",
-		width:"100%",
-	},
-});
