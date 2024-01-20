17637a3396436905d65b8ef16c1beba797537bb7
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-20 12:35:30 +0600

    refactor: move style to another file

---
+++ src/screens/Scorecard/MatchDetails/style.js
@@ -0,0 +1,93 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop:15,
+		margin:2,
+		padding:10,    
+	},
+
+	tableHeader: {
+		backgroundColor:'green',
+		flexWrap:'nowrap',
+		height:"10%",
+		width:"100%",
+		paddingTop:10,
+		paddingBottom:10,
+		paddingLeft:2,
+		flex:3,
+	},
+
+	tableHeaderText:{
+		color: "white",
+		fontWeight:"bold",
+		textAlign:'center',
+	},
+
+	tableRow: {
+		flexDirection: "row",
+		height: 40,
+		alignItems:"center",
+		flexWrap:"wrap",
+	},
+
+	rowItemName: {
+		alignItems:"center",
+		height:35,
+		margin:2,
+		padding:2,
+		justifyContent:'center',
+		flexWrap:"nowrap", 
+		flex:3,
+		flexShrink:1,
+	},
+
+	rowItemNameText: {
+		width:"100%",
+		height:"100%",
+		fontWeight: 'bold',
+		textAlign:"left",
+		fontSize:10,
+		paddingLeft:8,
+		justifyContent:'center',
+		flexWrap:'nowrap',
+		alignContent:'center',
+		flexDirection:'column',     
+	},
+
+	rowItemValue: {
+		alignItems:"center",
+		height:35,
+		margin:2,
+		padding:2,
+		justifyContent:'center',
+		flexWrap:"nowrap", 
+		flex:7,
+		flexShrink:1,
+	},
+
+	rowItemValueText: {
+		width:"100%",
+		height:"100%",
+		fontWeight: 'bold',
+		textAlign:"center",
+		fontSize:10,
+		justifyContent:'center',
+		flexWrap:'nowrap',
+		alignContent:'center',
+		flexDirection:'column',     
+	},
+
+	eachTableRowView: {
+		flex:2,
+		flexDirection:'row',
+		justifyContent:'space-between',
+		alignItems:'center',
+		flexWrap:'wrap',
+		height:"10%",
+		width:"100%",
+	},
+});
