d714067e07ad3fff3524b9de7e0f654c9bd7ba40
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-01-20 12:36:14 +0600

    refactor: move style to another file

---
+++ src/screens/Videos/VideoList/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop:15,
+		margin:2,
+		padding:10,
+	},
+
+	headline: {
+		fontSize:20,
+		fontWeight:'bold',
+		paddingTop:2,
+		marginTop:0,
+		marginBottom:30,
+	}
+});
