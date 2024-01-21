1154dec80ad7bd623f5755f4aa2caf0fb9eaf561
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-21 21:12:57 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/halloffame.js
+++ redux/reducers/halloffame.js
@@ -6,29 +6,29 @@ import {
 } from '../actions/HallOfFame';
 
 const hallOfFame = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_HALL_OF_FAME:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_HALL_OF_FAME:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_HALL_OF_FAME:
-            return true;
-        case actions.SUCCESS_FETCH_HALL_OF_FAME:
-        case actions.FAILURE_FETCH_HALL_OF_FAME:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_HALL_OF_FAME:
+			return true;
+		case actions.SUCCESS_FETCH_HALL_OF_FAME:
+		case actions.FAILURE_FETCH_HALL_OF_FAME:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    hallOfFame,
-    isFetching,
+	hallOfFame,
+	isFetching,
 });
 
 export const getHallOfFame = state => state.hallOfFame || [];
