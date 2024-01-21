7f4d1e4a0f752a63487812f40148f03bb7c28677
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-21 20:49:22 +0600

    chore: remove spaces from indentation and replace with tabs

--- redux/reducers/blog.js
+++ redux/reducers/blog.js
@@ -1,79 +1,77 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 
 import {
-    actions
+	actions
 } from '../action-types/blog';
 
 const blogs = (state = [], action) => {
-        switch (action.type) {
-            case actions.SUCCESS_FETCH_BLOGS:
-                return action.response;
-            default:
-                return state;
-        }
+	switch (action.type) {
+        case actions.SUCCESS_FETCH_BLOGS:
+            return action.response;
+        default:
+            return state;
+	}
 };
 
 const blog = (state = [], action) => {
-        switch (action.type) {
-            case actions.SUCCESS_FETCH_BLOG_BY_SLUG:
-            case actions.SUCCESS_FETCH_BLOG_BY_ID:
-                return [action.response];
-            default:
-                return state;
-        }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_BLOG_BY_SLUG:
+		case actions.SUCCESS_FETCH_BLOG_BY_ID:
+			return [action.response];
+		default:
+			return state;
+    }
 };
 
 const isFetchingIndividual = (state = false, action) => {
-        switch (action.type) {
-            case actions.REQUEST_FETCH_BLOG_BY_ID:
-            case actions.REQUEST_FETCH_BLOG_BY_SLUG:
-                return true;
-            case actions.SUCCESS_FETCH_BLOG_BY_ID:
-            case actions.FAILURE_FETCH_BLOG_BY_ID:
-            case actions.SUCCESS_FETCH_BLOG_BY_SLUG:
-            case actions.FAILURE_FETCH_BLOG_BY_SLUG:
-                return false;
-            default:
-                return state;
-        }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_BLOG_BY_ID:
+		case actions.REQUEST_FETCH_BLOG_BY_SLUG:
+			return true;
+		case actions.SUCCESS_FETCH_BLOG_BY_ID:
+		case actions.FAILURE_FETCH_BLOG_BY_ID:
+		case actions.SUCCESS_FETCH_BLOG_BY_SLUG:
+		case actions.FAILURE_FETCH_BLOG_BY_SLUG:
+			return false;
+		default:
+			return state;
+	}
    
 };
 
 const count = (state = 0, action) => {
-        switch (action.type) {
-            case actions.SUCCESS_COUNT_BLOG:
-                return action.response;
-            default:
-                return state;
-        }
-  
+	switch (action.type) {
+		case actions.SUCCESS_COUNT_BLOG:
+			return action.response;
+		default:
+			return state;
+	}
 }
 
 const isFetching = (state = false, action) => {
-        switch (action.type) {
-            case actions.REQUEST_FETCH_BLOGS:
-            case actions.REQUEST_CREATE_BLOG:
-            case actions.REQUEST_UPDATE_BLOG:
-            case actions.REQUEST_DELETE_BLOG:
-            case actions.REQUEST_COUNT_BLOG:
-                return true;
-            case actions.SUCCESS_UPDATE_BLOG:
-            case actions.FAILURE_UPDATE_BLOG:
-            case actions.SUCCESS_DELETE_BLOG:
-            case actions.FAILURE_DELETE_BLOG:
-            case actions.FAILURE_COUNT_BLOG:
-            case actions.SUCCESS_COUNT_BLOG:
-            case actions.SUCCESS_CREATE_BLOG:
-            case actions.FAILURE_CREATE_BLOG:
-            case actions.SUCCESS_FETCH_BLOGS:
-            case actions.FAILURE_FETCH_BLOGS:
-                return false;
-            default:
-                return state;
-        }
-
+	switch (action.type) {
+		case actions.REQUEST_FETCH_BLOGS:
+		case actions.REQUEST_CREATE_BLOG:
+		case actions.REQUEST_UPDATE_BLOG:
+		case actions.REQUEST_DELETE_BLOG:
+		case actions.REQUEST_COUNT_BLOG:
+			return true;
+		case actions.SUCCESS_UPDATE_BLOG:
+		case actions.FAILURE_UPDATE_BLOG:
+		case actions.SUCCESS_DELETE_BLOG:
+		case actions.FAILURE_DELETE_BLOG:
+		case actions.FAILURE_COUNT_BLOG:
+		case actions.SUCCESS_COUNT_BLOG:
+		case actions.SUCCESS_CREATE_BLOG:
+		case actions.FAILURE_CREATE_BLOG:
+		case actions.SUCCESS_FETCH_BLOGS:
+		case actions.FAILURE_FETCH_BLOGS:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
