e0de6b0a420bbc48950d8622f73ba3ef74131df4
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-21 21:12:12 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/counter.js
+++ redux/reducers/counter.js
@@ -6,61 +6,61 @@ import {
 } from '../actions/Counter';
 
 const counters = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_COUNTERS:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_COUNTERS:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const counter = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_COUNTER_BY_ID:
-            return [action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_COUNTER_BY_ID:
+			return [action.response];
+		default:
+			return state;
+	}
 };
 
 const isFetchingIndividual = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_COUNTER_BY_ID:
-            return true;
-        case actions.SUCCESS_FETCH_COUNTER_BY_ID:
-        case actions.FAILURE_FETCH_COUNTER_BY_ID:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_COUNTER_BY_ID:
+			return true;
+		case actions.SUCCESS_FETCH_COUNTER_BY_ID:
+		case actions.FAILURE_FETCH_COUNTER_BY_ID:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_COUNTERS:
-        case actions.REQUEST_CREATE_COUNTER:
-        case actions.REQUEST_UPDATE_COUNTER:
-        case actions.REQUEST_DELETE_COUNTER:
-            return true;
-        case actions.SUCCESS_UPDATE_COUNTER:
-        case actions.FAILURE_UPDATE_COUNTER:
-        case actions.SUCCESS_DELETE_COUNTER:
-        case actions.FAILURE_DELETE_COUNTER:
-        case actions.SUCCESS_CREATE_COUNTER:
-        case actions.FAILURE_CREATE_COUNTER:
-        case actions.SUCCESS_FETCH_COUNTERS:
-        case actions.FAILURE_FETCH_COUNTERS:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_COUNTERS:
+		case actions.REQUEST_CREATE_COUNTER:
+		case actions.REQUEST_UPDATE_COUNTER:
+		case actions.REQUEST_DELETE_COUNTER:
+			return true;
+		case actions.SUCCESS_UPDATE_COUNTER:
+		case actions.FAILURE_UPDATE_COUNTER:
+		case actions.SUCCESS_DELETE_COUNTER:
+		case actions.FAILURE_DELETE_COUNTER:
+		case actions.SUCCESS_CREATE_COUNTER:
+		case actions.FAILURE_CREATE_COUNTER:
+		case actions.SUCCESS_FETCH_COUNTERS:
+		case actions.FAILURE_FETCH_COUNTERS:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    counter,
-    counters,
-    isFetching,
-    isFetchingIndividual
+	counter,
+	counters,
+	isFetching,
+	isFetchingIndividual
 });
 
 export const getCounters = state => state.counters || [];
