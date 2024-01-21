d9fd340a70243931cb7d640be74773c2eb978106
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-21 13:19:40 +0600

    refactor: fix indentation

--- src/screens/Scorecard/ScorecardScreen.js
+++ src/screens/Scorecard/ScorecardScreen.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
 import { 
-  useSelector, 
+	useSelector, 
 } from 'react-redux';
 import { SafeAreaView } from 'react-native';
+import { ScrollView } from 'react-native';
+
 import Batting from "./Batting";
 import Bowling from "./Bowling";
 import MatchDetails from "./MatchDetails";
 import Squad from "./Squad"; 
-
 import { styles } from "./Scorecard.style";
-import { ScrollView } from 'react-native';
 import { getInningsEs } from '../../../redux/reducers';
 import { getMatchDetails } from '../../../redux/reducers';
 import { getMatch} from '../../../redux/reducers';
@@ -17,9 +17,9 @@ import { dfs } from '../../API/utils';
 
 const ScorecardComponent = ({ route }) => {
 
-  const {slug} = route.params;
+	const {slug} = route.params;
 
-  const inningses = dfs(useSelector(
+	const inningses = dfs(useSelector(
 		state => getInningsEs(state, slug)
 	));
 
@@ -31,10 +31,10 @@ const ScorecardComponent = ({ route }) => {
 		state => getMatch(state, slug)
 	);
 
-  return (
-    <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>{
-        (
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView style={styles.scrollView}>
+				{(
 					inningses
 					&&
 					Array.isArray(
@@ -52,29 +52,27 @@ const ScorecardComponent = ({ route }) => {
 						innings.over
 					)
 						&& (
-              <Fragment>
-                <Batting 
-                  batting={innings.batting} 
-                  didnotbat={innings.didnotbat} 
-                  extras={innings.extras} 
-                  total={innings.inningstotal} 
-                  fow={innings.fow}
-                  out={innings.out}
-                  over={dfs(innings.over)}
-                />
-                <Bowling 
-                  bowling={innings.bowling}
-                />
-              </Fragment>
-          
-            )
-        )}
-          <MatchDetails {...matchDetails}/>
-          <Squad teams={data.teams}/>
-        </ScrollView>  
-    </SafeAreaView>
-  );
+					<Fragment>
+						<Batting 
+							batting={innings.batting} 
+							didnotbat={innings.didnotbat} 
+							extras={innings.extras} 
+							total={innings.inningstotal} 
+							fow={innings.fow}
+							out={innings.out}
+							over={dfs(innings.over)}
+						/>
+						<Bowling 
+							bowling={innings.bowling}
+						/>
+					</Fragment>
+					)
+				)}
+        		<MatchDetails {...matchDetails}/>
+				<Squad teams={data.teams}/>
+			</ScrollView>  
+		</SafeAreaView>
+	);
 }; 
-
 
 export default ScorecardComponent;
