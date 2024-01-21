8af6f76537279a2ec7b6ee415d8b1b6de02df48e
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-21 21:13:52 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/livescore.js
+++ redux/reducers/livescore.js
@@ -2,105 +2,105 @@ import { combineReducers } from "redux";
 import { actions } from "../action-types/livescore";
 
 const liveScores = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_LIVESCORES:
-            return action.response.data;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_LIVESCORES:
+			return action.response.data;
+		default:
+			return state;
+	}
 };
 
 const upcoming = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_UPCOMING:
-            return action.response.data;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_UPCOMING:
+			return action.response.data;
+		default:
+			return state;
+	}
 };
 
 const bdMatch = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_BD_MATCH:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_BD_MATCH:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const liveScoresTest = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_LIVESCORES_TEST:
-            return action.response.data;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_LIVESCORES_TEST:
+			return action.response.data;
+		default:
+			return state;
+	}
 };
 
 const liveScoresRecentFinished = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_LIVESCORES_RECENT_FINISHED:
-            return action.response.data;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_LIVESCORES_RECENT_FINISHED:
+			return action.response.data;
+		default:
+			return state;
+	}
 };
 
 const isFetchingLivescores = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_LIVESCORES:
-        case actions.REQUEST_FETCH_BD_MATCH:
-        case actions.REQUEST_FETCH_LIVESCORES_TEST:
-        case actions.REQUEST_FETCH_UPCOMING:
-        case actions.REQUEST_FETCH_LIVESCORES_RECENT_FINISHED:
-            return true;
-        case actions.SUCCESS_FETCH_LIVESCORES:
-        case actions.SUCCESS_FETCH_BD_MATCH:
-        case actions.SUCCESS_FETCH_LIVESCORES_TEST:
-        case actions.SUCCESS_FETCH_LIVESCORES_RECENT_FINISHED:
-        case actions.FAILURE_FETCH_LIVESCORES:
-        case actions.SUCCESS_FETCH_UPCOMING:
-        case actions.FAILURE_FETCH_BD_MATCH:
-        case actions.FAILURE_FETCH_LIVESCORES_TEST:
-        case actions.FAILURE_FETCH_LIVESCORES_RECENT_FINISHED:
-        case actions.FAILURE_FETCH_UPCOMING:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_LIVESCORES:
+		case actions.REQUEST_FETCH_BD_MATCH:
+		case actions.REQUEST_FETCH_LIVESCORES_TEST:
+		case actions.REQUEST_FETCH_UPCOMING:
+		case actions.REQUEST_FETCH_LIVESCORES_RECENT_FINISHED:
+			return true;
+		case actions.SUCCESS_FETCH_LIVESCORES:
+		case actions.SUCCESS_FETCH_BD_MATCH:
+		case actions.SUCCESS_FETCH_LIVESCORES_TEST:
+		case actions.SUCCESS_FETCH_LIVESCORES_RECENT_FINISHED:
+		case actions.FAILURE_FETCH_LIVESCORES:
+		case actions.SUCCESS_FETCH_UPCOMING:
+		case actions.FAILURE_FETCH_BD_MATCH:
+		case actions.FAILURE_FETCH_LIVESCORES_TEST:
+		case actions.FAILURE_FETCH_LIVESCORES_RECENT_FINISHED:
+		case actions.FAILURE_FETCH_UPCOMING:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    liveScores,
-    bdMatch,
-    upcoming,
-    liveScoresTest,
-    liveScoresRecentFinished,
-    isFetchingLivescores,
+	liveScores,
+	bdMatch,
+	upcoming,
+	liveScoresTest,
+	liveScoresRecentFinished,
+	isFetchingLivescores,
 })
 
 export const getLiveScoresHome = state => {
-    let scores = (
-        (
-            state.liveScoresTest
-            &&
-            state.liveScores
-        )
-        &&
-        state.liveScores
-            .concat(
-                state.liveScoresTest
-            )
-    )
+	let scores = (
+		(
+			state.liveScoresTest
+			&&
+			state.liveScores
+		)
+		&&
+		state.liveScores
+			.concat(
+				state.liveScoresTest
+			)
+	)
 
-        ||
-        (
-            state.liveScores
-            ||
-            []
-        );
+		||
+		(
+			state.liveScores
+			||
+			[]
+		);
 
-    return scores.filter((l, index) => index < 5).map(l => l)
+	return scores.filter((l, index) => index < 5).map(l => l)
 };
 
 export const getLiveScoresRecentFinished = state => state.liveScoresRecentFinished || [];
