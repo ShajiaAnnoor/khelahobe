4efb92e85f469089f7373c99a70b33c06192cab9
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:06:53 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/rank.js
+++ redux/reducers/rank.js
@@ -1,61 +1,61 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../actions/Rank';
 
 const rank = (state = {
-    team: {},
-    batting: {},
-    bowling: {},
-    'all-rounder': {},
+	team: {},
+	batting: {},
+	bowling: {},
+	'all-rounder': {},
 }, action) => {
-    switch (action.type) {
-        case actions.SUCCESS_TEAM_RANK:
-            return {
-                ...state,
-                team: {
-                    ...state.team,
-                    [action.rtype]: [
-                        ...action.response,
-                    ],
-                },
-            };
-        case actions.SUCCESS_PLAYER_RANK:
-            return {
-                ...state,
-                [action.dtype]: {
-                    ...state[action.dtype],
-                    [action.rtype]: [
-                        ...action.response,
-                    ],
-                },
-            };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_TEAM_RANK:
+			return {
+				...state,
+				team: {
+					...state.team,
+					[action.rtype]: [
+						...action.response,
+					],
+				},
+			};
+		case actions.SUCCESS_PLAYER_RANK:
+			return {
+				...state,
+				[action.dtype]: {
+					...state[action.dtype],
+					[action.rtype]: [
+						...action.response,
+					],
+				},
+			};
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_TEAM_RANK:
-        case actions.REQUEST_PLAYER_RANK:
-            return true;
-        case actions.SUCCESS_TEAM_RANK:
-        case actions.FAILURE_TEAM_RANK:
-        case actions.SUCCESS_PLAYER_RANK:
-        case actions.FAILURE_PLAYER_RANK:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_TEAM_RANK:
+		case actions.REQUEST_PLAYER_RANK:
+			return true;
+		case actions.SUCCESS_TEAM_RANK:
+		case actions.FAILURE_TEAM_RANK:
+		case actions.SUCCESS_PLAYER_RANK:
+		case actions.FAILURE_PLAYER_RANK:
+			return false;
+		default:
+			return state;
+	}
 };
 
 
 export default combineReducers({
-    rank,
-    isFetching,
+	rank,
+	isFetching,
 });
 
 export const getRank = (state, group, type) => state.rank[group][type];
