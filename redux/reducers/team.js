e4703d1496ad51de4f5b1587e23260cabc49975f
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:07:51 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/team.js
+++ redux/reducers/team.js
@@ -1,231 +1,231 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions,
+	actions,
 } from '../actions/Team';
 
 const intro = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_INTRO:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_INTRO:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_INTRO:
-            return true;
-        case actions.RECEIVE_INTRO:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_INTRO:
+			return true;
+		case actions.RECEIVE_INTRO:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const results = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_RESULTS:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_RESULTS:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingResults = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_RESULTS:
-            return true;
-        case actions.RECEIVE_RESULTS:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_RESULTS:
+			return true;
+		case actions.RECEIVE_RESULTS:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const schedule = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_SCHEDULE:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_SCHEDULE:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingSchedule = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_SCHEDULE:
-            return true;
-        case actions.RECEIVE_SCHEDULE:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_SCHEDULE:
+			return true;
+		case actions.RECEIVE_SCHEDULE:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const table = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_TABLE:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_TABLE:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingTable = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_TABLE:
-            return true;
-        case actions.RECEIVE_TABLE:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_TABLE:
+			return true;
+		case actions.RECEIVE_TABLE:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const odi = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_ODI_SQUAD:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_ODI_SQUAD:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingODI = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_ODI_SQUAD:
-            return true;
-        case actions.RECEIVE_ODI_SQUAD:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_ODI_SQUAD:
+			return true;
+		case actions.RECEIVE_ODI_SQUAD:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const t20 = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_T20_SQUAD:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_T20_SQUAD:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingT20 = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_T20_SQUAD:
-            return true;
-        case actions.RECEIVE_T20_SQUAD:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_T20_SQUAD:
+			return true;
+		case actions.RECEIVE_T20_SQUAD:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const test = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_TEST_SQUAD:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_TEST_SQUAD:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingTest = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_TEST_SQUAD:
-            return true;
-        case actions.RECEIVE_TEST_SQUAD:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_TEST_SQUAD:
+			return true;
+		case actions.RECEIVE_TEST_SQUAD:
+			return false;
+		default:
+			return state;
+	}
 }
 
 const playerData = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_PLAYER_BASIC:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_PLAYER_BASIC:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingPlayerData = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_PLAYER_BASIC:
-            return true;
-        case actions.RECEIVE_PLAYER_BASIC:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_PLAYER_BASIC:
+			return true;
+		case actions.RECEIVE_PLAYER_BASIC:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const highestScorer = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_HIGHEST_SCORER:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_HIGHEST_SCORER:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingHighestScorer = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_HIGHEST_SCORER:
-            return true;
-        case actions.RECEIVE_HIGHEST_SCORER:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_HIGHEST_SCORER:
+			return true;
+		case actions.RECEIVE_HIGHEST_SCORER:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const highestWicketTaker = (state = [], action) => {
-    switch (action.type) {
-        case actions.RECEIVE_HIGHEST_WICKET_TAKER:
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.RECEIVE_HIGHEST_WICKET_TAKER:
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 const isFetchingHighestWicketTaker = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_WICKET_TAKER:
-            return true;
-        case actions.RECEIVE_WICKET_TAKER:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_WICKET_TAKER:
+			return true;
+		case actions.RECEIVE_WICKET_TAKER:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    intro,
-    isFetching,
-    results,
-    isFetchingResults,
-    schedule,
-    isFetchingSchedule,
-    table,
-    isFetchingTable,
-    odi,
-    isFetchingODI,
-    t20,
-    isFetchingT20,
-    test,
-    isFetchingTest,
-    playerData,
-    isFetchingPlayerData,
-    highestScorer,
-    isFetchingHighestScorer,
-    highestWicketTaker,
-    isFetchingHighestWicketTaker,
+	intro,
+	isFetching,
+	results,
+	isFetchingResults,
+	schedule,
+	isFetchingSchedule,
+	table,
+	isFetchingTable,
+	odi,
+	isFetchingODI,
+	t20,
+	isFetchingT20,
+	test,
+	isFetchingTest,
+	playerData,
+	isFetchingPlayerData,
+	highestScorer,
+	isFetchingHighestScorer,
+	highestWicketTaker,
+	isFetchingHighestWicketTaker,
 });
 
 export const getIntro = state => state.intro;
