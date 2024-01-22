3a993d7e4538194e26352758e7f4940f34397baf
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:08:25 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/translation.js
+++ redux/reducers/translation.js
@@ -1,79 +1,79 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../actions/Translation';
 
 const translations = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_TRANSLATION:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_TRANSLATION:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const translationIndividual = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_TRANSLATION_BY_ID:
-            return [action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_TRANSLATION_BY_ID:
+			return [action.response];
+		default:
+			return state;
+	}
 };
 
 const isFetchingIndividual = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_TRANSLATION_BY_ID:
-            return true;
-        case actions.SUCCESS_FETCH_TRANSLATION_BY_ID:
-        case actions.FAILURE_FETCH_TRANSLATION_BY_ID:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_TRANSLATION_BY_ID:
+			return true;
+		case actions.SUCCESS_FETCH_TRANSLATION_BY_ID:
+		case actions.FAILURE_FETCH_TRANSLATION_BY_ID:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_TRANSLATION:
-        case actions.REQUEST_CREATE_TRANSLATION:
-        case actions.REQUEST_UPDATE_TRANSLATION:
-        case actions.REQUEST_DELETE_TRANSLATION:
-        case actions.REQUEST_COUNT_TRANSLATION:
-            return true;
-        case actions.SUCCESS_UPDATE_TRANSLATION:
-        case actions.FAILURE_UPDATE_TRANSLATION:
-        case actions.SUCCESS_COUNT_TRANSLATION:
-        case actions.FAILURE_COUNT_TRANSLATION:
-        case actions.SUCCESS_DELETE_TRANSLATION:
-        case actions.FAILURE_DELETE_TRANSLATION:
-        case actions.SUCCESS_CREATE_TRANSLATION:
-        case actions.FAILURE_CREATE_TRANSLATION:
-        case actions.SUCCESS_FETCH_TRANSLATION:
-        case actions.FAILURE_FETCH_TRANSLATION:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_TRANSLATION:
+		case actions.REQUEST_CREATE_TRANSLATION:
+		case actions.REQUEST_UPDATE_TRANSLATION:
+		case actions.REQUEST_DELETE_TRANSLATION:
+		case actions.REQUEST_COUNT_TRANSLATION:
+			return true;
+		case actions.SUCCESS_UPDATE_TRANSLATION:
+		case actions.FAILURE_UPDATE_TRANSLATION:
+		case actions.SUCCESS_COUNT_TRANSLATION:
+		case actions.FAILURE_COUNT_TRANSLATION:
+		case actions.SUCCESS_DELETE_TRANSLATION:
+		case actions.FAILURE_DELETE_TRANSLATION:
+		case actions.SUCCESS_CREATE_TRANSLATION:
+		case actions.FAILURE_CREATE_TRANSLATION:
+		case actions.SUCCESS_FETCH_TRANSLATION:
+		case actions.FAILURE_FETCH_TRANSLATION:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const count = (state = 0, action) => {
-    switch (action.type) {
-        case actions.SUCCESS_COUNT_TRANSLATION:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_COUNT_TRANSLATION:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    count,
-    translations,
-    isFetching,
-    isFetchingIndividual,
-    translationIndividual,
+	count,
+	translations,
+	isFetching,
+	isFetchingIndividual,
+	translationIndividual,
 });
 
 export const getTranslations = state => state.translations || [];
