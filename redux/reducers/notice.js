378a195c652d8fb04ae26940247e9107cd043148
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:05:54 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/notice.js
+++ redux/reducers/notice.js
@@ -1,66 +1,66 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../actions/Notice';
 
 const notices = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_NOTICES:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_NOTICES:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const notice = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_NOTICE_BY_ID:
-            return [action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_NOTICE_BY_ID:
+			return [action.response];
+		default:
+			return state;
+	}
 };
 
 const isFetchingIndividual = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_NOTICE_BY_ID:
-            return true;
-        case actions.SUCCESS_FETCH_NOTICE_BY_ID:
-        case actions.FAILURE_FETCH_NOTICE_BY_ID:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_NOTICE_BY_ID:
+			return true;
+		case actions.SUCCESS_FETCH_NOTICE_BY_ID:
+		case actions.FAILURE_FETCH_NOTICE_BY_ID:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_NOTICES:
-        case actions.REQUEST_CREATE_NOTICE:
-        case actions.REQUEST_UPDATE_NOTICE:
-        case actions.REQUEST_DELETE_NOTICE:
-            return true;
-        case actions.SUCCESS_UPDATE_NOTICE:
-        case actions.FAILURE_UPDATE_NOTICE:
-        case actions.SUCCESS_DELETE_NOTICE:
-        case actions.FAILURE_DELETE_NOTICE:
-        case actions.SUCCESS_CREATE_NOTICE:
-        case actions.FAILURE_CREATE_NOTICE:
-        case actions.SUCCESS_FETCH_NOTICES:
-        case actions.FAILURE_FETCH_NOTICES:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_NOTICES:
+		case actions.REQUEST_CREATE_NOTICE:
+		case actions.REQUEST_UPDATE_NOTICE:
+		case actions.REQUEST_DELETE_NOTICE:
+			return true;
+		case actions.SUCCESS_UPDATE_NOTICE:
+		case actions.FAILURE_UPDATE_NOTICE:
+		case actions.SUCCESS_DELETE_NOTICE:
+		case actions.FAILURE_DELETE_NOTICE:
+		case actions.SUCCESS_CREATE_NOTICE:
+		case actions.FAILURE_CREATE_NOTICE:
+		case actions.SUCCESS_FETCH_NOTICES:
+		case actions.FAILURE_FETCH_NOTICES:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    notice,
-    notices,
-    isFetching,
-    isFetchingIndividual
+	notice,
+	notices,
+	isFetching,
+	isFetchingIndividual
 });
 
 export const getNotices = state => state.notices || [];
