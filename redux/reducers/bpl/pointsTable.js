7653c6f9024c3afdc09182aedd8c4408aa943692
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:28:30 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/bpl/pointsTable.js
+++ redux/reducers/bpl/pointsTable.js
@@ -6,30 +6,30 @@ import {
 } from '../../actions/PointsTable';
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_BPL_POINTS_TABLE:
-            return true;
-        case actions.SUCCESS_BPL_POINTS_TABLE:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_BPL_POINTS_TABLE:
+			return true;
+		case actions.SUCCESS_BPL_POINTS_TABLE:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const pointstable = (state = {}, action) => {
-    switch (action.type) {
-        case actions.SUCCESS_BPL_POINTS_TABLE:
-            return {
-                ...action.response,
-            };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_BPL_POINTS_TABLE:
+			return {
+				...action.response,
+			};
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    pointstable,
-    isFetching,
+	pointstable,
+	isFetching,
 });
 
 export const getIsFetching = state => state.isFetching;
