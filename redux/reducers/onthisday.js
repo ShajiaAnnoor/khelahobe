ea442d75c284692cba55bffe4274218519061950
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:06:15 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/onthisday.js
+++ redux/reducers/onthisday.js
@@ -1,71 +1,71 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../actions/OnThisDay';
 
 const onThisDay = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_ON_THIS_DAY:
-            return action.response;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_ON_THIS_DAY:
+			return action.response;
+		default:
+			return state;
+	}
 };
 
 const onThisDayById = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_SLUG:
-            return [action.response];
-        case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_ID:
-            return [action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_SLUG:
+			return [action.response];
+		case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_ID:
+			return [action.response];
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_ON_THIS_DAY:
-        case actions.REQUEST_CREATE_ON_THIS_DAY:
-        case actions.REQUEST_UPDATE_ON_THIS_DAY:
-        case actions.REQUEST_DELETE_ON_THIS_DAY:
-        case actions.REQUEST_FETCH_ALL_ON_THIS_DAY:
-            return true;
-        case actions.SUCCESS_UPDATE_ON_THIS_DAY:
-        case actions.FAILURE_UPDATE_ON_THIS_DAY:
-        case actions.SUCCESS_FETCH_ALL_ON_THIS_DAY:
-        case actions.FAILURE_FETCH_ALL_ON_THIS_DAY:
-        case actions.SUCCESS_DELETE_ON_THIS_DAY:
-        case actions.FAILURE_DELETE_ON_THIS_DAY:
-        case actions.SUCCESS_CREATE_ON_THIS_DAY:
-        case actions.FAILURE_CREATE_ON_THIS_DAY:
-        case actions.SUCCESS_FETCH_ON_THIS_DAY:
-        case actions.FAILURE_FETCH_ON_THIS_DAY:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_ON_THIS_DAY:
+		case actions.REQUEST_CREATE_ON_THIS_DAY:
+		case actions.REQUEST_UPDATE_ON_THIS_DAY:
+		case actions.REQUEST_DELETE_ON_THIS_DAY:
+		case actions.REQUEST_FETCH_ALL_ON_THIS_DAY:
+			return true;
+		case actions.SUCCESS_UPDATE_ON_THIS_DAY:
+		case actions.FAILURE_UPDATE_ON_THIS_DAY:
+		case actions.SUCCESS_FETCH_ALL_ON_THIS_DAY:
+		case actions.FAILURE_FETCH_ALL_ON_THIS_DAY:
+		case actions.SUCCESS_DELETE_ON_THIS_DAY:
+		case actions.FAILURE_DELETE_ON_THIS_DAY:
+		case actions.SUCCESS_CREATE_ON_THIS_DAY:
+		case actions.FAILURE_CREATE_ON_THIS_DAY:
+		case actions.SUCCESS_FETCH_ON_THIS_DAY:
+		case actions.FAILURE_FETCH_ON_THIS_DAY:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const isFetchingIndividual = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_ON_THIS_DAY_BY_ID:
-            return true;
-        case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_ID:
-        case actions.FAILURE_FETCH_ON_THIS_DAY_BY_ID:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_ON_THIS_DAY_BY_ID:
+			return true;
+		case actions.SUCCESS_FETCH_ON_THIS_DAY_BY_ID:
+		case actions.FAILURE_FETCH_ON_THIS_DAY_BY_ID:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    onThisDay,
-    onThisDayById,
-    isFetching,
-    isFetchingIndividual
+	onThisDay,
+	onThisDayById,
+	isFetching,
+	isFetchingIndividual
 });
 
 export const getOnThisDayAll = state => state.onThisDay || [];
