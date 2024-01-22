96c3f30eb3dc3a9ec309a4b25aa0e90582bf4e5b
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:28:47 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/bpl/teamList.js
+++ redux/reducers/bpl/teamList.js
@@ -1,35 +1,35 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../../actions/Bpl';
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_BPL_TEAM_LIST:
-            return true;
-        case actions.SUCCESS_BPL_TEAM_LIST:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_BPL_TEAM_LIST:
+			return true;
+		case actions.SUCCESS_BPL_TEAM_LIST:
+			return false;
+		default:
+			return state;
+	}
 };
 
 const teamList = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_BPL_TEAM_LIST:
-            return [
-                ...action.response,
-            ];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_BPL_TEAM_LIST:
+			return [
+				...action.response,
+			];
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    teamList,
-    isFetching,
+	teamList,
+	isFetching,
 });
 
 export const getIsFetching = state => state.isFetching;
