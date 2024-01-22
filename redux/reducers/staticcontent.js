d2d3ccd5c08a57c18a2de130e026e1c3011c39a7
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-22 23:07:18 +0600

    chore: remove spaces and add tabs for indentation

--- redux/reducers/staticcontent.js
+++ redux/reducers/staticcontent.js
@@ -1,61 +1,61 @@
 import {
-    combineReducers
+	combineReducers
 } from 'redux';
 import {
-    actions
+	actions
 } from '../actions/StaticContent';
 
 const staticContents = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_STATIC_CONTENTS:
-            return action.response;
-        // case actions.SUCCESS_DELETE_STATIC_CONTENT:
-        //     del = state.STATIC_CONTENT.find(i => i.id === id);
-        //     delete del;
-        //     return state;
-        // case actions.SUCCESS_FETCH_STATIC_CONTENT:
-        //     return [...state, action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_STATIC_CONTENTS:
+			return action.response;
+		// case actions.SUCCESS_DELETE_STATIC_CONTENT:
+		//     del = state.STATIC_CONTENT.find(i => i.id === id);
+		//     delete del;
+		//     return state;
+		// case actions.SUCCESS_FETCH_STATIC_CONTENT:
+		//     return [...state, action.response];
+		default:
+			return state;
+	}
 };
 
 const staticContentIndividual = (state = [], action) => {
-    switch (action.type) {
-        case actions.SUCCESS_FETCH_STATIC_CONTENT_BY_ID:
-            return [action.response];
-        case actions.SUCCESS_FETCH_STATIC_CONTENT_BY_SLUG:
-            return [action.response];
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.SUCCESS_FETCH_STATIC_CONTENT_BY_ID:
+			return [action.response];
+		case actions.SUCCESS_FETCH_STATIC_CONTENT_BY_SLUG:
+			return [action.response];
+		default:
+			return state;
+	}
 };
 
 const isFetching = (state = false, action) => {
-    switch (action.type) {
-        case actions.REQUEST_FETCH_STATIC_CONTENTS:
-        case actions.REQUEST_CREATE_STATIC_CONTENT:
-        case actions.REQUEST_UPDATE_STATIC_CONTENT:
-        case actions.REQUEST_DELETE_STATIC_CONTENT:
-            return true;
-        case actions.SUCCESS_UPDATE_STATIC_CONTENT:
-        case actions.FAILURE_UPDATE_STATIC_CONTENT:
-        case actions.SUCCESS_DELETE_STATIC_CONTENT:
-        case actions.FAILURE_DELETE_STATIC_CONTENT:
-        case actions.SUCCESS_CREATE_STATIC_CONTENT:
-        case actions.FAILURE_CREATE_STATIC_CONTENT:
-        case actions.SUCCESS_FETCH_STATIC_CONTENTS:
-        case actions.FAILURE_FETCH_STATIC_CONTENTS:
-            return false;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actions.REQUEST_FETCH_STATIC_CONTENTS:
+		case actions.REQUEST_CREATE_STATIC_CONTENT:
+		case actions.REQUEST_UPDATE_STATIC_CONTENT:
+		case actions.REQUEST_DELETE_STATIC_CONTENT:
+			return true;
+		case actions.SUCCESS_UPDATE_STATIC_CONTENT:
+		case actions.FAILURE_UPDATE_STATIC_CONTENT:
+		case actions.SUCCESS_DELETE_STATIC_CONTENT:
+		case actions.FAILURE_DELETE_STATIC_CONTENT:
+		case actions.SUCCESS_CREATE_STATIC_CONTENT:
+		case actions.FAILURE_CREATE_STATIC_CONTENT:
+		case actions.SUCCESS_FETCH_STATIC_CONTENTS:
+		case actions.FAILURE_FETCH_STATIC_CONTENTS:
+			return false;
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-    staticContents,
-    staticContentIndividual,
-    isFetching,
+	staticContents,
+	staticContentIndividual,
+	isFetching,
 });
 
 export const getStaticContents = state => (state.staticContents && state.staticContents) || [];
