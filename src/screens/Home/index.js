53ec7f90617cffea0529de652c04fb81b7cd5a12
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 16:11:32 +0600

    chore: remove spaces and add tabs for indentation

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -10,55 +10,54 @@ const UpcomingScore = lazy(() => import('./Upcoming.js'));
 const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() { 
-    return ( 
-        <Tab.Navigator 
-            initialRouteName="LiveScore" 
-            screenOptions={({ route }) => ({
-            headerShown: false, 
-            tabBarShowIcon: true, 
-            tabBarStyle: 
-            { 
-                backgroundColor: '#FFFFFF', 
-                borderColor: '#efc96c', 
-            }, 
-            tabBarActiveTintColor: 'green', 
-            tabBarInactiveTintColor: '#A29B92',
-                
-            tabBarIndicatorStyle: { 
-                backgroundColor: 'green', 
-                borderTopRightRadius: 999, 
-                borderTopLeftRadius: 999, 
-            },
+	return ( 
+		<Tab.Navigator 
+			initialRouteName="LiveScore" 
+			screenOptions={({ route }) => ({
+			headerShown: false, 
+			tabBarShowIcon: true, 
+			tabBarStyle: 
+			{ 
+				backgroundColor: '#FFFFFF', 
+				borderColor: '#efc96c', 
+			}, 
+			tabBarActiveTintColor: 'green', 
+			tabBarInactiveTintColor: '#A29B92',
+				
+			tabBarIndicatorStyle: { 
+				backgroundColor: 'green', 
+				borderTopRightRadius: 999, 
+				borderTopLeftRadius: 999, 
+			},
 
-            tabBarItemStyle: { width: 140}, 
-            
-            tabBarLabel: ({ focused, color }) => ( 
-                <Text 
-                    style={
-                        [(focused ? {fontWeight: "bold"} : {fontWeight: "normal"}), {color: color}]}
-                >{
-                    route.name}
-                </Text> 
-            ), 
-            tabBarScrollEnabled: true })} 
-        >                 
-            <Tab.Screen 
-                name="LiveScore" 
-                component={LiveScore} 
-                options={{ tabBarLabel: "লাইভ স্কোর" }} 
-            />
-            <Tab.Screen 
-                name="Upcoming" 
-                component={UpcomingScore} 
-                options={{ tabBarLabel: "আসন্ন" }} 
-            />
-            <Tab.Screen 
-                name="Result" 
-                component={FinishedScore} 
-                options={{ tabBarLabel: "ফলাফল" }} 
-            /> 
-        </Tab.Navigator> 
-    );
+			tabBarItemStyle: { width: 140}, 
+			
+			tabBarLabel: ({ focused, color }) => ( 
+				<Text 
+					style={
+						[(focused ? {fontWeight: "bold"} : {fontWeight: "normal"}), {color: color}]}
+				>{
+					route.name}
+				</Text> 
+			), 
+			tabBarScrollEnabled: true })} 
+		>                 
+			<Tab.Screen 
+				name="LiveScore" 
+				component={LiveScore} 
+				options={{ tabBarLabel: "লাইভ স্কোর" }} 
+			/>
+			<Tab.Screen 
+				name="Upcoming" 
+				component={UpcomingScore} 
+				options={{ tabBarLabel: "আসন্ন" }} 
+			/>
+			<Tab.Screen 
+				name="Result" 
+				component={FinishedScore} 
+				options={{ tabBarLabel: "ফলাফল" }} 
+			/> 
+		</Tab.Navigator> 
+	);
 }
 export default function App() { return ( <MyTabs /> );}
-
