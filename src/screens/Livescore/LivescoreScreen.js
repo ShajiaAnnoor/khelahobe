92a814dc8aa1102f843d09740d882d8ceb8f47ff
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 16:12:03 +0600

    chore: remove redundant imports

--- src/screens/Livescore/LivescoreScreen.js
+++ src/screens/Livescore/LivescoreScreen.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { 
-  useSelector, 
-  useDispatch,
-} from 'react-redux';
-import { 
   SafeAreaView, 
-  View,
 } from 'react-native';
 
 import { styles } from "./Livescore.style";
