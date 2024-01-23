efbef525a8cde8c3c65b779d4e3cb2bbbdc6d6a7
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 16:14:25 +0600

    chore: remove spaces and add tabs for indentation

--- src/screens/News/NewsScreen.js
+++ src/screens/News/NewsScreen.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { withStyles } from '@ui-kitten/components';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 import { styles } from "./News.style";
 
-const NewsScreen = ({ navigation }) => {
-
-    return (
-        <SafeAreaView style={styles.container}>
-        </SafeAreaView>
-  );
+const NewsScreen = ({ }) => {
+	return (
+		<SafeAreaView style={styles.container}>
+		</SafeAreaView>
+	);
 };
 
 export default NewsScreen;
