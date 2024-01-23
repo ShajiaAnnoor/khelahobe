f97a617de8da374b5735d5c38c6abc1563a8776e
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 15:46:11 +0600

    chore: remove spaces and add tabs for indentation

--- src/API/livescoreboard.js
+++ src/API/livescoreboard.js
@@ -3,30 +3,30 @@ import apiUrls from "./apiurls";
 import { dfs, processMatch, processInnings } from "./utils";
 
 const fetchMatch = ({ slug }) => axiosGet(apiUrls.FETCH_MATCH, {}, slug).then(
-    response => dfs(processMatch(response.data)),
-    err => Promise.reject(() => ({
-        message: err,
-    }))
+	response => dfs(processMatch(response.data)),
+	err => Promise.reject(() => ({
+		message: err,
+	}))
 );
 
 const fetchInnings = ({ slug, inningsNo }) => axiosGet(apiUrls.FETCH_INNINGS, {}, slug, inningsNo).then(
-    response => dfs(processInnings(response.data)),
-    err => Promise.reject(() => ({
-        message: err,
-    }))
+	response => dfs(processInnings(response.data)),
+	err => Promise.reject(() => ({
+		message: err,
+	}))
 );
 
 const fetchEndOfOver = ({ slug }) => axiosGet(apiUrls.FETCH_END_OF_OVER, {}, slug).then(
-    response => console.log(response) || response.data,
-    err => {
-        return Promise.reject(() => ({
-            message: err,
-        }));
-    }
+	response => console.log(response) || response.data,
+	err => {
+		return Promise.reject(() => ({
+			message: err,
+		}));
+	}
 );
 
 export const endPoints = {
-    FETCH_MATCH: fetchMatch,
-    FETCH_INNINGS: fetchInnings,
-    FETCH_END_OF_OVER: fetchEndOfOver,
+	FETCH_MATCH: fetchMatch,
+	FETCH_INNINGS: fetchInnings,
+	FETCH_END_OF_OVER: fetchEndOfOver,
 }
