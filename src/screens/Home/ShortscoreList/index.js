7e417a585a3d015f6da9115f697666838f2ce7a3
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 16:08:01 +0600

    refactor: separate style to another function

--- src/screens/Home/ShortscoreList/index.js
+++ src/screens/Home/ShortscoreList/index.js
@@ -7,15 +7,15 @@ import LiveScoreInfo from '../LivescoreInfo';
 const flag = 1;
 
 const ShortscoreList = ({ livescores }) => { 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={livescores}
-                renderItem={({ item }) => <LiveScoreInfo slug={item.slug[0]} flag={flag} />}
-                keyExtractor={item => item.slug[0]}
-            />
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={livescores}
+				renderItem={({ item }) => <LiveScoreInfo slug={item.slug[0]} flag={flag} />}
+				keyExtractor={item => item.slug[0]}
+			/>
+		</View>
+	)
 };
 
 export default ShortscoreList;
