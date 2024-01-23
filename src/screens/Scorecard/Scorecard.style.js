7097faecb73f2324fa9a3af263ae8fbf0ee2672f
Shajia Annoor <shajia.annoor@gmail.com>
2024-01-23 16:20:30 +0600

    chore: remove spaces and add tabs for indentation

--- src/screens/Scorecard/Scorecard.style.js
+++ src/screens/Scorecard/Scorecard.style.js
@@ -1,15 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 20,
-      justifyContent:"space-evenly",
-      flexDirection:'column',
-    },
-    scrollView: {
-    },
-    text: {
-      fontSize: 42,
-    },
+	container: {
+		flex: 1,
+		paddingTop: 20,
+		justifyContent:"space-evenly",
+		flexDirection:'column',
+	},
+	scrollView: {
+	},
+	text: {
+		fontSize: 42,
+	},
 });
