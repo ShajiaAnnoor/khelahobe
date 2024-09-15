dee7509c862691a62f81e3076473cbb508dc236d
Shajia Annoor <shajiaannoor@gmail.com>
2024-09-15 14:47:17 +0600

    feat: add async functions for getting match related data

--- src/API/livescore.js
+++ src/API/livescore.js
@@ -52,6 +52,61 @@ const fetchFininshed = () => axiosGet(apiUrls.FETCH_FINISHED, {}).then(
     }
 );
 
+const fetchMatch = ({ slug }) => axiosGet(apiUrls.FETCH_MATCH, {}, slug).then(
+    response => processMatch(response.data),
+    err => Promise.reject(() => ({
+        message: err,
+    }))
+);
+
+
+const fetchInnings = ({ slug, inningsNo }) => axiosGet(apiUrls.FETCH_INNINGS, {}, slug, inningsNo).then(
+    response => processInnings(response.data),
+    err => Promise.reject(() => ({
+        message: err,
+    }))
+);
+const fetchEndOfOver = ({ slug }) => axiosGet(apiUrls.FETCH_END_OF_OVER, {}, slug).then(
+    response => response.data,
+    err => {
+        return Promise.reject(() => ({
+            message: err,
+        }));
+    }
+);
+const fetchLocalMatches = () => axiosGet(apiUrls.FETCH_LOCAL_MATCHES, {}).then(
+    response => response, //different from other, check out the reducer for better understanding
+    err => {
+        return Promise.reject(() => ({
+            message: err,
+        }));
+    }
+);
+const fetchInternationalMatches = () => axiosGet(apiUrls.FETCH_INTERNATIONAL_MATCHES, {}).then(
+    response => response, //different from other, check out the reducer for better understanding
+    err => {
+        return Promise.reject(() => ({
+            message: err,
+        }));
+    }
+);
+
+const fetchLive = () => axiosGet(apiUrls.FETCH_LIVE, {}).then(
+    response => response, //different from other, check out the reducer for better understanding
+    err => {
+        return Promise.reject(() => ({
+            message: err,
+        }));
+    }
+);
+
+const fetchMatchWithInnings = ({ slug }) => axiosGet(apiUrls.FETCH_MATCH_WITH_INNINGS, {}, slug).then(
+    response => dfs(processMatch(response.data)),
+    err => Promise.reject(() => ({
+        message: err,
+    }))
+);
+
 export const endPoints = {
 	FETCH_LIVESCORES: fetchLiveScores,
 	FETCH_LIVESCORES_TEST: fetchLiveScoresTest,
@@ -59,4 +114,11 @@ export const endPoints = {
 	FETCH_BD_MATCH: fetchBDMatch,
 	FETCH_UPCOMING: fetchUpcoming,
 	FETCH_FINISHED: fetchFininshed,
+	FETCH_LIVE: fetchLive,
+    FETCH_LOCAL_MATCHES: fetchLocalMatches,
+    FETCH_INTERNATIONAL_MATCHES: fetchInternationalMatches,
+	FETCH_MATCH: fetchMatch,
+    FETCH_INNINGS: fetchInnings,
+    FETCH_END_OF_OVER: fetchEndOfOver,
+    FETCH_MATCH_WITH_INNINGS: fetchMatchWithInnings,
 }
